01c94f93e8e0b67a1c59dbbf52eaa74ddd91f803
tbrou <thomas.broussard@gmail.com>
2018-12-12 18:45:28 +0100

    * dao introduction

---
+++ BankAccountSystem/src/fr/epita/bank/service/CustomerFileDAO.java
@@ -0,0 +1,27 @@
+package fr.epita.bank.service;
+
+import java.util.List;
+
+import fr.epita.bank.datamodel.Customer;
+
+public class CustomerFileDAO {
+	
+	public void create(Customer customer) {
+		
+	}
+	public void update(Customer customer) {
+		
+	}
+	public void delete(Customer customer) {
+		
+	}
+	public List<Customer> search(Customer criteria) {
+		return null;
+	}
+	
+	public Customer getById(String name) {
+		return null;
+	}
+	
+
+}
